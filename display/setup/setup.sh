dc4fa3d3579f3f42e07fe408eb4f0baf037ce902
markome-beep <pirro.marko1@gmail.com>
2026-02-13 20:43:56 -0600

    setup.sh test

--- display/setup/setup.sh
+++ display/setup/setup.sh
@@ -1,45 +1,46 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade
-sudo apt install -y --no-install-recommends \
-        unclutter-fixes \
-        xserver-xorg \
-        xinit \
-        mpv \
-        fastfetch \
-        socat \
-        openssh-server
+# sudo apt update
+# sudo apt upgrade
+# sudo apt install -y --no-install-recommends \
+#         unclutter-fixes \
+#         xserver-xorg \
+#         xinit \
+#         mpv \
+#         fastfetch \
+#         socat \
+#         openssh-server
 #        openbox \
 #        wezterm \
 
-cat <<EOF > ~/.xinitrc
-#!/bin/sh
-
+# cat <<EOF > ~/.xinitrc
+# #!/bin/sh
+#
+# # xset -dpms
+# # xset s off
+# # xset s noblank
+#
+# unclutter -idle 1 &
+#
+# # wezterm --config-file ~/.config/wezterm.lua start -- fastfetch &
+# exec <my-server>
+# EOF
+#
+# cat <<EOF > ~/.xinitrc
+# #!/bin/sh
+#
 # xset -dpms
 # xset s off
 # xset s noblank
-
-unclutter -idle 1 &
-
+#
+# unclutter -idle 1 &
+#
+# openbox &
 # wezterm --config-file ~/.config/wezterm.lua start -- fastfetch &
-exec <my-server>
-EOF
+# exec <my-server>
+# EOF
 
-cat <<EOF > ~/.xinitrc
-#!/bin/sh
-
-xset -dpms
-xset s off
-xset s noblank
-
-unclutter -idle 1 &
-
-openbox &
-wezterm --config-file ~/.config/wezterm.lua start -- fastfetch &
-exec <my-server>
-EOF
-
+# chmod +x ~/.xinitrc
 
 # cat <<EOF > ~/.config/wezterm.lua
 # -- Pull in the wezterm API
@@ -61,17 +62,32 @@ EOF
 # return config
 # EOF
 
+read -p "Enter a number (10-255): " num
+
+# Check if it's an integer
+if ! [[ "$num" =~ ^[0-9]+$ ]]; then
+  echo "Error: Not a valid number." >&2
+  exit 1
+fi
+
+# Check range
+if (( num < 10 || num > 255 )); then
+  echo "Error: Number must be between 10 and 255." >&2
+  exit 1
+fi
+
 NET_FILE="/etc/network/interfaces"
 cat <<EOF >> "$NET_FILE"
 
 # Virtual Static IP (Fallback/Secondary)
 auto eth0:0
 iface eth0:0 inet static
-address 192.168.1.200
+address 192.168.1.$num
 netmask 255.255.255.0
 EOF
 
-chmod +x ~/.xinitrc
-
-
-
+mkdir -p ~/.ssh
+cat <<EOF >> "$HOME/.ssh/authorized_keys"
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFi3bU1NHLQU56N08qbxIJAS8/gVJAzt/vr8Q20Zmx63 bluerachapradit@SOE-MAC-AW6Q6LR
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAc0jA2C50dJ9zZbyjXVmlD0x5TvnblKVm1PxRqnPFJ8 markome@nixos
+EOF
--- display/setup/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo echo "here"
-sudo echo "again"
