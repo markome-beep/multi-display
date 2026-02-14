f8a7aa35f9c0ee0d3a1066c584ace744ead439da
markome-beep <pirro.marko1@gmail.com>
2026-02-14 13:51:52 -0600

    updated autostart

--- display/setup/setup.sh
+++ display/setup/setup.sh
@@ -52,7 +52,7 @@ sudo tee -a "$HOME/.config/autostart/my-mpv.desktop" > /dev/null <<EOF
 [Desktop Entry]
 Type=Application
 Name=my-mpv
-Exec= mpv --input-ipc-server=/tmp/mpv-socket --fullscreen --no-border --video-sync=display-resample /home/$USER/output.mpv | tee /home/$USER/mpv-log
+Exec=mpv --input-ipc-server=/tmp/mpv-socket --fullscreen --no-border --idle | tee /home/$USER/mpv-log
 Terminal=true
 EOF
 
